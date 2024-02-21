417bde7d79e54d86536280b963809dc1f8bb1397
Alvino Albas <2111522016_alvino@student.unand.ac.id>
2024-02-21 14:16:12 +0700

    update

--- leader/dist/config/koneksi.php
+++ leader/dist/config/koneksi.php
@@ -4,7 +4,7 @@
   $dbhost = "localhost";
   $dbuser = "root";
   $dbpass = "";
-  $dbname = "db_cuti";
+  $dbname = "cuti";
 
   $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Tidak dapat terhubung ke database: ".mysqli_error());
 ?>
