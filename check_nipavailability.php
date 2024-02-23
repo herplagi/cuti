0e7083c1536fb979828403b7151bfabc347970ed
Diqzi <daprialdi11@gmail.com>
2024-02-23 08:24:39 +0700

    up

--- check_nipavailability.php
+++ check_nipavailability.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 require_once("dist/config/koneksi.php");
 // code user username availablity
-if(!empty($_POST["nip"])) {
-	$nip= $_POST["nip"];
+if (!empty($_POST["nip"])) {
+	$nip = $_POST["nip"];
 	$sql = "SELECT * FROM employee WHERE nip='$nip'";
-	$query = mysqli_query($conn,$sql);
-	if(mysqli_num_rows($query)>0){
-		echo "<span style='color:red'> nip sudah terdaftar.</span>";
+	$query = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($query) > 0) {
+		echo "<span style='color:red'> NIP sudah terdaftar.</span>";
 		echo "<script>$('#submit').prop('disabled',true);</script>";
-	}else{
-		echo "<span style='color:green'> nip bisa digunakan.</span>";
+	} else {
+		echo "<span style='color:green'> NIP bisa digunakan.</span>";
 		echo "<script>$('#submit').prop('disabled',false);</script>";
 	}
 }
